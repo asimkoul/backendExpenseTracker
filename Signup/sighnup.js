865ed169ee091803a7787552110bdcecb5a45b25
asimkoul <ahmadasim080gmail.com>
2024-05-04 15:45:01 +0530

    first

---
+++ Signup/sighnup.js
@@ -0,0 +1,21 @@
+async function sighnup(event){
+    try {
+        event.preventDefault()
+        const sighnupDetails={
+            name:event.target.name.value,
+            email:event.target.email.value,
+            password:event.target.password.value
+        }
+        const response=await axios.post("http://localhost:3000/user/sighnup",sighnupDetails)
+        if(response.status===201){
+            window.location.href="../Login/login.html"
+        }
+        else{
+            throw new Error("Failed to login")
+        }
+        
+    } catch (error) {
+        document.body.innerHTML=`<div style="color:red;">${error} </div>`;
+    }
+   
+}
